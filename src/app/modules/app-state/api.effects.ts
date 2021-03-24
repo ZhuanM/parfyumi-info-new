594d513d679b6286740762d1e035bede64f7699b
Zhuan <jowan@abv.bg>
2021-03-24 08:54:36 +0200

    comments added

--- src/app/modules/app-state/api.effects.ts
+++ src/app/modules/app-state/api.effects.ts
@@ -5,6 +5,8 @@ import { switchMap } from 'rxjs/operators';
 import { Fragrances } from 'src/app/utils/constants';
 import { of } from 'rxjs';
 
+// LAZY LOADED MODULES TO BE ADDED IN THE FUTURE
+// 'any' : Provides a unique instance in each lazy loaded module while all eagerly loaded modules share one instance.
 @Injectable({ providedIn: 'any' })
 export class ApiEffects {
 
